36231a0bae61021f7b7a84cf96c61fa96b0eb089
Raphael Ndolo <raphaelndolo@EBLMICTMT2RQR0GCV.local>
2025-03-20 20:39:17 +0300

    Using Data Layer to view all students information.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,18 +10,26 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getAllStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "ndoloraphael@gmail.com",
-                        LocalDate.of(1984, Month.AUGUST, 8),
-                        "Male",
-                        "Raphael",
-                        "Charles",
-                        "7577",
-                        LocalDate.of(2000, Month.JANUARY, 8)
-                )
-        );
+        return studentRepository.findAll();
+//        return List.of(
+//                new Student(
+//                        1L,
+//                        "ndoloraphael@gmail.com",
+//                        LocalDate.of(1984, Month.AUGUST, 8),
+//                        "Male",
+//                        "Raphael",
+//                        "Charles",
+//                        "7577",
+//                        LocalDate.of(2000, Month.JANUARY, 8)
+//                )
+//        );
     }
 }
